0495d1e0b26b46c10f3150337a6ac018ee60ed7d
August Eriksson <103457657+eriks-son@users.noreply.github.com>
2024-05-26 17:57:12 -0400

    Add For Me to songs

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -10,6 +10,7 @@ function Music({navbarSelected}) {
     );
     const readMoreMusicShowText = "Click to read more about some amazing musical opportunities I've had!"
     const songsIvePlayed = [
+        `<iframe style={{"border-radius": "12px"}} src="https://open.spotify.com/embed/track/3zejtdOOa90ObNXH92gvhW?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         `<iframe style={{"border-radius": "12px"}} src="https://open.spotify.com/embed/track/0731deLwmQsvfvHmfL0Vup?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         `<iframe style={{"border-radius": "12px"}} src="https://open.spotify.com/embed/track/3DPunWJcudK88nW03nFioE?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
     ];
